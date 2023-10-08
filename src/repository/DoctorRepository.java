5f656eb2038a99e65473aef464204f0c1ae1c8bb
luc <hotanluc19@gmail.com>
2023-10-08 10:34:42 +0700

    doctormanager

---
+++ src/repository/DoctorRepository.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+import java.util.ArrayList;
+import model.Doctor;
+
+/**
+ *
+ * @author PC
+ */
+public class DoctorRepository {
+
+    public void addDoctor(ArrayList<Doctor> ld) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void updateDoctor(ArrayList<Doctor> ld) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void deleteDoctor(ArrayList<Doctor> ld) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void searchDoctor(ArrayList<Doctor> ld) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
